6a7b36216f5dcac5ca3ee4a197d979eedfcdd2b4
1024小神 <15670339118@163.com>
2025-04-23 13:35:22 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.oaikes.pakeplus.android"
+        applicationId = "com.app.pakeplus.android"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
